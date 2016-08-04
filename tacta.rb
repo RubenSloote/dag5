0464161372f91f106acb366147f8f6723202c1c3
RubenSloote <rubensloote@gmail.com>
2016-08-04 11:34:37 +0200

    create contacts

--- tacta.rb
+++ tacta.rb
@@ -10,19 +10,6 @@ contacts.each_with_index do |contact, i|
    puts "#{i + 1}) #{contact[:name]}"
 end
 
-puts
-print "Who would you like to see? "
-response = gets.chomp
-
-i = response.to_i
-
-contact = contacts[i-1]
-
-puts
-puts "#{contact[:name]}"
-puts "phone: #{contact[:phone]}"
-puts "email: #{contact[:email]}"
-
 def ask( prompt )
    print prompt
    gets.chomp
@@ -31,34 +18,47 @@ end
 
 def index( contacts )
 
-  puts
-  response = ask "Who would you like to see? "
-
-  i = response.to_i
-
-  contact = contacts[i-1]
-
-  puts
-  show( contact )
-
-
 end
 
 loop do
    index( contacts )
 
    puts
-   response = ask "Who would you like to see (q to quit)? "
+   response = ask "Who would you like to see (n for new, q to quit)? "
 
    break if response == "q"
 
-   i = response.to_i
+   if response == "n"
+      contact = {}
 
-   contact = contacts[i-1]
+      puts
+      puts "Enter contact info:"
 
-   puts
-   show( contact )
-   puts
+      contact[:name ] = ask "Name? "
+      contact[:phone] = ask "Phone? "
+      contact[:email] = ask "Email? "
+
+      contacts << contact
+
+      puts
+      puts "New contact created:"
+      puts
+
+      show( contact )
+      puts
+   else
+      i = response.to_i
+
+      contact = contacts[i-1]
+
+      puts
+      show( contact )
+      puts
+   end
+
 end
+
+
+
  puts
  puts "Bye!"
